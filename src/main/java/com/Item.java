afd801daa79586632ae96c289f9e72cf5fa2b2a4
Lucas Nascimento <nascimento.ti@gmail.com>
2019-09-10 20:18:05 -0300

    new item should avoid cost greater than 100

--- src/main/java/com/Item.java
+++ src/main/java/com/Item.java
@@ -14,11 +14,69 @@ public class Item {
     private Float weigh;
     private Float cost;
 
+    /**
+     * Constructor's Item
+     *
+     * @param index of an item
+     * @param weigh of an item
+     * @param cost og on item
+     * @throws APIException if not Max weight and cost of an item is ≤ 100
+     */
     public Item(Integer index, Float weigh, Float cost) throws APIException {
         if (!FloatValidator.getInstance().maxValue(weigh, 100))
             throw new APIException("Max weigh is 100");
+        if (!FloatValidator.getInstance().maxValue(cost, 100))
+            throw new APIException(("Max cost is 100"));
         this.index = index;
         this.weigh = weigh;
+        this.cost = cost;
+    }
+
+    /**
+     * Index's getter
+     * @return index
+     */
+    public Integer getIndex() {
+        return index;
+    }
+
+    /**
+     * Index's setter
+     * @param index of an item
+     */
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    /**
+     * Weigh's getter
+     * @return weigh
+     */
+    public Float getWeigh() {
+        return weigh;
+    }
+
+    /**
+     * Weigh's setter
+     * @param weigh of an item
+     */
+    public void setWeigh(Float weigh) {
+        this.weigh = weigh;
+    }
+
+    /**
+     * Cost's getter
+     * @return cost
+     */
+    public Float getCost() {
+        return cost;
+    }
+
+    /**
+     * Cost's setter
+     * @param cost of an item
+     */
+    public void setCost(Float cost) {
         this.cost = cost;
     }
 }
--- src/test/java/mobiquityinc/model/ItemTest.java
+++ src/test/java/mobiquityinc/model/ItemTest.java
@@ -11,4 +11,9 @@ public class ItemTest {
         new Item(1, 101.0f, 1.0f);
     }
 
+    @Test(expected = APIException.class)
+    public void should_avoid_cost_greater_than_100() throws APIException {
+        new Item(1, 100.0f, 101.0f );
+    }
+
 }
