3143fbba678a11a33ac02b606e5c3db5240ccb39
Lucas Nascimento <nascimento.ti@gmail.com>
2019-09-10 22:20:12 -0300

    shoud read all file correct

--- src/main/java/com/mobiquityinc/utils/ReaderHelper.java
+++ src/main/java/com/mobiquityinc/utils/ReaderHelper.java
@@ -1,5 +1,15 @@
 package com.mobiquityinc.utils;
 
+import com.mobiquityinc.exception.APIException;
+import com.mobiquityinc.model.Item;
+import com.mobiquityinc.model.Package;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Utility class to read all data from input file format's
  *
@@ -15,6 +25,55 @@ public class ReaderHelper {
      */
     static boolean validateLine(String line) {
         return line.split(" ") .length >= 3 ;
+    }
+
+    /**
+     * Utility method to read holy file
+     *
+     * @param path to input file
+     * @return List of packages
+     * @throws APIException if something went wrong
+     */
+    static List<Package> readFile(String path) throws APIException {
+        File file = new File(path);
+        List<Package> packages = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String inputLine = scanner.nextLine();
+                packages.add(parseLine(inputLine));
+            }
+        } catch (Exception e) {
+            throw new APIException("Something went wrong reading file", e);
+        }
+
+        return packages;
+    }
+
+    /**
+     * Translate line into Package Model Class
+     *
+     * @param line to be parsed
+     * @return parsed Package
+     * @throws APIException if format is invalid
+     */
+    static private Package parseLine(String line) throws APIException {
+        if (!validateLine(line)) {
+            throw new APIException("Invalid format");
+        }
+        String tokens[] = line.split(" ");
+        Float packageWeigh = Float.parseFloat(tokens[0]);
+        List<Item> items = new LinkedList<>();
+
+        for (int i = 2 ; i < tokens.length ; i++){
+            String[] split = tokens[i].replaceAll("[()€]", "").split(",");
+            Integer index = Integer.valueOf(split[0]);
+            Float weight = Float.valueOf(split[1]);
+            Float cost = Float.valueOf(split[2]);
+            items.add(new Item(index, weight, cost));
+        }
+
+        return new Package(packageWeigh, items);
     }
 
 
--- src/test/java/com/mobiquityinc/utils/ReaderHelperTest.java
+++ src/test/java/com/mobiquityinc/utils/ReaderHelperTest.java
@@ -1,8 +1,12 @@
 package com.mobiquityinc.utils;
 
 import com.mobiquityinc.exception.APIException;
+import com.mobiquityinc.model.Package;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
 
 public class ReaderHelperTest {
 
@@ -18,4 +22,11 @@ public class ReaderHelperTest {
         Assert.assertTrue( ReaderHelper.validateLine(wrongFormat));
     }
 
+    @Test()
+    public void should_read_all_file() throws APIException {
+        File sampleFile = new File("src/test/resources/sample-input-file.txt");
+        List<Package> packages = ReaderHelper.readFile(sampleFile.getAbsolutePath());
+
+        Assert.assertTrue(packages.size() ==  4);
+    }
 }
